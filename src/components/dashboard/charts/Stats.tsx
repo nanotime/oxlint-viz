673dcdf3b55ef25c7e389df70862ef2ee1965eca
Javier Marin <nano.world.contact@gmail.com>
2026-04-11 16:16:25 -0600

    feat(charts): enhance StatCard with icon and description support

--- src/components/dashboard/charts/Stats.tsx
+++ src/components/dashboard/charts/Stats.tsx
@@ -1,7 +1,7 @@
-import { Component, For } from "solid-js";
+import { Component, For, JSX, Show } from "solid-js";
 
 interface Stats {
-  stats: Array<{ statTitle: string; stat: number }>;
+  stats: Array<{ statTitle: string; stat: number; icon?: JSX.Element; desc?: string }>;
 }
 
 export const StatCard: Component<Stats> = (props) => {
@@ -9,9 +9,15 @@ export const StatCard: Component<Stats> = (props) => {
     <div class="stats shadow">
       <For each={props.stats}>
         {(stat) => (
-          <div class="stat">
+          <div class="stat items-center">
+            <Show when={stat.icon}>
+              <div class="stat-figure">{stat.icon}</div>
+            </Show>
             <div class="stat-title">{stat.statTitle}</div>
             <div class="stat-value">{stat.stat}</div>
+            <Show when={stat.desc}>
+              <div class="stat-desc">{stat.desc}</div>
+            </Show>
           </div>
         )}
       </For>
